ae2198ed4c756725a9f33ff8e2317264e7ca6dc8
Keely Smith <keely.smith1@icloud.com>
2022-10-08 16:52:50 +1100

    minor change

--- src/Assignment3/Feedback.java
+++ src/Assignment3/Feedback.java
@@ -4,7 +4,7 @@ public class Feedback {
     private String type;
     private String content;
 
-    Feedback(String type, String content){
+    Feedback(String type, String content){ //create feedback with type (complaint or regular) and content
         this.type = type;
         this.content = content;
     }
